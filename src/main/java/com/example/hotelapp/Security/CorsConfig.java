80068fcd6fe62d919180aa7552972ecac84ac306
Roy-de <roymuuri28@gmail.com>
2023-06-30 13:08:05 +0300

    enabled cors

---
+++ src/main/java/com/example/hotelapp/Security/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.hotelapp.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class CorsConfig {
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
