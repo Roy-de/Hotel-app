3778ad10bdf75c111fff3f9b5f903b1bec0bd12b
Roy-de <roymuuri28@gmail.com>
2023-08-01 08:00:52 +0300

    final

--- src/main/java/com/example/hotelapp/Controller/Booking/BookingController.java
+++
@@ -1,9 +0,0 @@
-package com.example.hotelapp.Controller.Booking;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/book")
-public class BookingController {
-}
